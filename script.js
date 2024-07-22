50864ccdacf22a507d323adcdff1a6bc4b0adec5
imsunokdir <67774734+imsunokdir@users.noreply.github.com>
2024-07-22 14:07:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+
+function addBook(event) {
+	event.preventDefault()
+    
+	let tbody = document.querySelector("tbody");
+	let title = document.getElementById("title").value;
+	let author = document.getElementById("author").value;
+	let isbn = document.getElementById("isbn").value;
+	let data = [title, author, isbn,"X"]
+	let table = document.getElementsByTagName("table")[0];
+	let newTr = document.createElement("tr");
+
+	for(let i of data){
+		let newTd = document.createElement("td");
+		newTd.textContent = i;
+		newTr.appendChild(newTd);
+	}
+	
+	tbody.appendChild(newTr);
+	document.getElementById("title").value=""
+	document.getElementById("author").value=""
+	document.getElementById("isbn").value=""
+	
+	
+}
+
+
+
